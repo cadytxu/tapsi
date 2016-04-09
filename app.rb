8f159db83036040e2e1259027c4d41ad46e70cb0
cadytxu <cadytxu@gmail.com>
2016-04-09 08:37:29 +0000

    init website

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 
 class Tapsi < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Welcome to Tapsi</h1></body></html>"
   end
+  get '/button' do 
+    "<!DOCTYPE html><html><head></head><body><h1>Thanks for your donation on Watsi</h1></body></html>"
+  end 
 end
